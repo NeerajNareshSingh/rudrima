111960413d006668b473e32463359477ba1fc868
NeerajNareshSingh <singh.neeraj@rudrima.in>
2025-07-30 15:11:32 +0530

    Update index.html and style.css for improved layout and styling
    - Added Font Awesome for enhanced iconography in the navigation and testimonials.
    - Updated header navigation structure for better responsiveness and visual appeal.
    - Revised text content in various sections to reflect the new branding and messaging.
    - Introduced new background images for project showcases and testimonials.
    - Enhanced button styles and added new CSS classes for better user interaction.
    - Updated contact details and office location in the map.js file for accuracy.
    - Added multiple new images to support the updated content and branding.

--- js/map.js
+++ js/map.js
@@ -2,15 +2,15 @@
     MAP -->
     =============================================== */
     
-    var locations=[ ['<div class="infobox"><h3 class="title"><a href="#">OUR USA OFFICE</a></h3><span>NEW YORK CITY 2045 / 65</span><span>+90 555 666 77 88</span></div>',
-    52.370216,
-    4.895168,
+    var locations=[ ['<div class="infobox"><h3 class="title"><a href="#">OUR OFFICE</a></h3><span>PW93+2R Lucknow, Uttar Pradesh / 65</span><span>+095 80 94 49 23</span></div>',
+        26.7176093,
+        80.9045919,
     2]];
     var map=new google.maps.Map(document.getElementById('map'), {
         zoom: 14, scrollwheel: false, navigationControl: true, mapTypeControl: false, scaleControl: false, draggable: true, styles: [ {
             "featureType": "administrative", "elementType": "labels.text.fill", "stylers": [{"featureType":"poi.business","elementType":"geometry.fill","stylers":[{"visibility":"on"}]}]
         }
-        ], center: new google.maps.LatLng(52.370216, 4.895168), mapTypeId: google.maps.MapTypeId.ROADMAP
+        ], center: new google.maps.LatLng(26.7176093, 80.9045919), mapTypeId: google.maps.MapTypeId.ROADMAP
     }
     
     );
